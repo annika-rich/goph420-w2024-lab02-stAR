23ab4a89a9ea8612cf6765cc22a0be0a976f8499
Annika Richardson <annika.richardson@ucalgary.ca>
2024-03-22 19:10:28 -0600

    implemented newton_raphson algorithm

--- src/goph420_lab02/root_finding.py
+++ src/goph420_lab02/root_finding.py
@@ -1,25 +1,41 @@
 # Utility functions for Lab02
+import numpy as np 
 
 def root_newton_raphson(x0, f, dfdx):
-    """This function implements the Newton-Raphson Root Finding Open Method.
+	"""This function implements the Newton-Raphson Root Finding Open Method.
 
-    Parameters
-    ----------
-    x0:     float
-            initial guess for value of root
-    f:      function
-            Function for which root is evaluated
-    dfdx:   function
-            first derivative of function, f, for which the root is evaluated
+	Parameters
+	----------
+	x0:     float
+	initial guess for value of the root
+	f:      function
+	Function for which the root is evaluated
+	dfdx:   function
+	first derivative of function, f, for which the root is evaluated
 
-    Returns
-    -------
-    float
-    final estimate of the root of the given function
+	Returns
+	-------
+	float
+	final estimate of the root of the given function
 
-    int
-    number of iterations to convergence
+	int
+	number of iterations to convergence
 
-    numpy.ndarray
-    1D vector of approximate relative error at each iteration
-    """
+	numpy.ndarray
+	1D vector of approximate relative error at each iteration
+	"""
+	# initialize loop variables
+	tol = 1e-16 # approximate relative error toleranc
+	maxit = 100 # max iterations
+	esp_a = 2 * tol
+	itr = 1
+
+	while eps_a > tol & itr < maxit:
+		x1  = x0 - f(x0) / dfdx(x0) # Newton Raphson formula
+		eps_a = np.abs((x1 - x0) / x1) # calculate approx. relative error
+		x0 = x1 # update guess
+		if itr >= maxit:
+			raise RuntimeWarning(f"The Newton-Raphson Root Finding Algorithm did not converge over {itr} iterations.")
+		
+
+
